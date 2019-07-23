7448de9ed57a110280f22398a03eaecac5f2562d
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-07-23 20:20:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  enable :sessions 
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  register Sinatra::Flash
 
   get '/' do
     erb :index
